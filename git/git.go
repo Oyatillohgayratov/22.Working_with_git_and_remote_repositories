8eb7cb1ff1a15aa1097312b98eb336a6250ff65c
oyatilloh <azamatgayratov771@gmail.com>
2024-04-29 14:38:18 +0500

    GetUserEmail funktsiyasi va foydalanuvchi email fayli qo'shildi

--- git/git.go
+++ git/git.go
@@ -14,3 +14,11 @@ func GetUserName() (string, error) {
 	return string(d), nil
 }
 
+func GetUserEmail() (string, error) {
+	cmd := exec.Command("git", "config", "user.email")
+	d, error := cmd.Output()
+	if error != nil {
+		return "", fmt.Errorf("foydalanuvchi emaili  olinmadi: %v", error)
+	}
+	return string(d), nil
+}
