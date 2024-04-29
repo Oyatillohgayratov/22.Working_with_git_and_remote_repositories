e59027ae4d3ccc3d46fcab74074a221b1378ba9c
oyatilloh <azamatgayratov771@gmail.com>
2024-04-29 14:35:47 +0500

    GetUserName funktsiyasi va foydalanuvchi nomi fayli qo'shildi

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"homework/git"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.OpenFile("storage/username.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	name,err := git.GetUserName()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file.WriteString(name)
+
+	file.Close()
+
+}
